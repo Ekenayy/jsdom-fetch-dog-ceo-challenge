23f0b08b61f7da3357fb73d384cf5219960986a1
Ekenayy <ekenayy@gmail.com>
2020-12-16 17:16:28 -0500

    Challenge 3 completed

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
 const breedUrl = 'https://dog.ceo/api/breeds/list/all' 
 
+function toggleColor (element) {
+    let colorsArray = [
+        '#6b6bfa', 
+        '#fcb5fc',
+        '#ddff99',
+        '#ffff66', 
+        '#fa6b6b',
+    ]
+
+    let randomColor = colorsArray[Math.floor(Math.random() * colorsArray.length)];
+    element.target.style.color = randomColor
+    // debugger
+    // if (element.target.style.backgroundColor === 'rgb(255, 255, 102)' || element.target.style.backgroundColor === "rgb(221, 255, 153)") {
+    //     element.target.style.color = 'black'
+    // } else {
+    //     element.target.style.color = 'white'
+    // }
+ }
+
 let renderImage = (data) => {
     const imgDiv = document.querySelector('#dog-image-container')
     // ul = document.createElement('ul')
@@ -27,6 +46,10 @@ let renderBreed = (data) => {
     data.forEach((breed) => {
         li = document.createElement('li')
         li.textContent = breed
+        // debugger
+        li.addEventListener('click', (element) => {
+            toggleColor(element)
+        })
         ul.append(li)
     })
 
